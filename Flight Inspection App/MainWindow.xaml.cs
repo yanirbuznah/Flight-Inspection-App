5adc5b190b0bb2b3f19ce2eed8d5b56e73228d34
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2021-03-24 22:22:39 +0200

    Add project files.

---
+++ Flight Inspection App/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flight_Inspection_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HomePage home;
+        public MainWindow()
+        {
+            InitializeComponent();
+            home = new HomePage();
+            Navigate(home);
+        }
+    }
+}

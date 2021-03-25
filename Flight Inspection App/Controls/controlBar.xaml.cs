990167b021625791cdd38dc4409f9ba79f7d8b73
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2021-03-25 23:01:40 +0200

    Added control bar

---
+++ Flight Inspection App/Controls/controlBar.xaml.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Flight_Inspection_App.Controls
+{
+    /// <summary>
+    /// Interaction logic for controlBar.xaml
+    /// </summary>
+    public partial class controlBar : UserControl
+    {
+		private bool mediaPlayerIsPlaying = false;
+		private bool userIsDraggingSlider = false;
+		public controlBar()
+        {
+			InitializeComponent();
+
+			DispatcherTimer timer = new DispatcherTimer();
+			timer.Interval = TimeSpan.FromSeconds(1);
+			timer.Tick += timer_Tick;
+			timer.Start();
+		}
+
+
+
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+
+		}
+		private void Play_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			
+		}
+
+		private void Play_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+
+		}
+
+		private void Pause_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			
+		}
+
+		private void Pause_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+		
+		}
+
+		private void Stop_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = mediaPlayerIsPlaying;
+		}
+
+		private void Stop_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			
+			mediaPlayerIsPlaying = false;
+		}
+
+		private void sliProgress_DragStarted(object sender, DragStartedEventArgs e)
+		{
+			userIsDraggingSlider = true;
+		}
+
+		private void sliProgress_DragCompleted(object sender, DragCompletedEventArgs e)
+		{
+			userIsDraggingSlider = false;
+			
+		}
+
+		private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			lblProgressStatus.Text = TimeSpan.FromSeconds(sliProgress.Value).ToString(@"hh\:mm\:ss");
+		}
+
+		private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
+		{
+			
+		}
+
+        private void pbVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+
+}
